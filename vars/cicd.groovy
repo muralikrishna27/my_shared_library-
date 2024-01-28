623aa78157c5246b5f75666b3f8262258983a057
Murali Krishna27 <51109585+muralikrishna27@users.noreply.github.com>
2024-01-28 19:21:01 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -3,7 +3,12 @@ def newGit(repo)
   git "https://github.com/muralikrishna27/${repo}"
 }
 
-def neBuild()
+def newBuild()
 {
   sh 'mvn package'
+}
+
+def newDeploy(jobname,ip,context)
+{
+sh "/var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
